3cf80af15f3b7cc704dec9e821fe36b8e1e29c67
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-01 00:16:36 +0530

    UI fixes

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 var scores, roundScore, activePlayer, dice;
 
 score = [0,0];
@@ -53,9 +42,16 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];    
     
     //Check if the player won the Game
-    
-    nextPlayer();
+    if(score[activePlayer] >= 15){
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
         
+    }else{
+         //NextPlayer
+        nextPlayer();
+    }   
 });
 
 
